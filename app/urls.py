f5bde7f8a66ce8b8af351f1661926cde9b2cc9e1
pgh <parhamgh2020@github.com>
2023-03-26 19:56:48 +0330

    add urls

--- app/urls.py
+++ app/urls.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 from django.urls import path, include
 from . import views
 from rest_framework.routers import DefaultRouter
@@ -8,18 +9,16 @@ from .views import (BooksViewset,
 
 
 urlpatterns = [
-    path(eval(f'excer{i}'), eval(f'views.excer{i}')) for i in range(1, 41)
+    path(f'excer{i}/', eval(f'views.excer{i}')) for i in range(1, 41)
 ]
 
 
-# Create a router and register our viewsets with it.
 router = DefaultRouter()
-router.register(r'snippets', BooksViewset.as_view(), basename="book")
-router.register(r'snippets', AuthorsViewset.as_view(), basename="author")
-router.register(r'snippets', PublishersViewset.as_view(), basename="publisher")
-router.register(r'snippets', UsersViewset.as_view(), basename="user")
+router.register(r'book', BooksViewset, basename="book")
+router.register(r'author', AuthorsViewset, basename="author")
+router.register(r'publisher', PublishersViewset, basename="publisher")
+router.register(r'user', UsersViewset, basename="user")
 
-# The API URLs are now determined automatically by the router.
 urlpatterns += [
-    path('', include(router.urls)),
+    path('entity/', include(router.urls)),
 ]
